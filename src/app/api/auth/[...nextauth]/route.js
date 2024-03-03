4286fa949efa6b99780d5163e90d3ad404137634
aroob5166 <aroob1910132@akgec.ac.in>
2024-03-03 20:03:23 +0530

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "Iv1.e492703e6986543d",
+      clientSecret: "b31e44c69b4b9c6bc603a94c94a63ca991ea59da",
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session?.user?.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+
+      return session;
+    },
+  },
+  secret: "default_secret_key",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
